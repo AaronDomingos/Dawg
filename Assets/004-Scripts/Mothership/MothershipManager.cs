100fde4811e115c66a14a5573cbe5b5815244e8d
Tomas Pinal <tomas.pinal@teambespin.us>
2023-01-31 20:56:46 -0600

    Add FTL drive bar support

--- Assets/004-Scripts/Mothership/MothershipManager.cs
+++ Assets/004-Scripts/Mothership/MothershipManager.cs
@@ -3,11 +3,13 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MothershipManager : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI MateriumTxt;
     
+    public Slider ftlSlider;
     
     
     public float Health;
@@ -19,5 +21,7 @@ public class MothershipManager : MonoBehaviour
     private void FixedUpdate()
     {
         MateriumTxt.text = Convert.ToString(Materium);
+        ftlSlider.value = WarpDrive;
     }
+
 }
